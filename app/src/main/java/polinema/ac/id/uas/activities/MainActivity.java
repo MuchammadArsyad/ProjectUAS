accd1056c98f432d75ce8c45a08a73ea58347047
Muchammad Arsyad <arsyad1822@gmail.com>
2019-12-19 00:34:41 +0700

    update
    package activities dan fragments

--- app/src/main/java/polinema/ac/id/uas/activities/MainActivity.java
+++ app/src/main/java/polinema/ac/id/uas/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package polinema.ac.id.uas;
+package polinema.ac.id.uas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import polinema.ac.id.uas.R;
 
 public class MainActivity extends AppCompatActivity {
 
