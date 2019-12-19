b18aeb9c89d6583543f522ae2bbca31d5692e48b
Muchammad Arsyad <arsyad1822@gmail.com>
2019-12-19 09:10:32 +0700

    update MainActivity

--- app/src/main/java/polinema/ac/id/uas/activities/MainActivity.java
+++ app/src/main/java/polinema/ac/id/uas/activities/MainActivity.java
@@ -1,10 +1,19 @@
 package polinema.ac.id.uas.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import polinema.ac.id.uas.R;
+import polinema.ac.id.uas.fragments.JadwalFragment;
+import polinema.ac.id.uas.fragments.StadionFragment;
+import polinema.ac.id.uas.fragments.PemainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
